2e58e19ee6cf672c336f4608527a6b666e9576da
Abdeljalil Misbah <aj.msb91@gmail.com>
2025-05-18 12:14:44 +0300

    Vico Library added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,8 +39,12 @@ android {
     }
 }
 
-dependencies {
 
+
+dependencies {
+    // Vico Library
+    implementation(libs.vico.compose)
+    
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
