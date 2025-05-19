5cd5bdf4d02f19ae13cb9a04aba4af2cf260cc17
Abdeljalil Misbah <aj.msb91@gmail.com>
2025-05-19 13:27:16 +0300

    fixed isues

--- app/src/main/java/com/example/practicaltrainingproject/MainActivity.kt
+++ app/src/main/java/com/example/practicaltrainingproject/MainActivity.kt
@@ -12,9 +12,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.example.practicaltrainingproject.ui.theme.PracticalTrainingProjectTheme
+import com.patrykandpatrick.vico.compose.axis.horizontal.rememberBottomAxis
+import com.patrykandpatrick.vico.compose.axis.vertical.rememberStartAxis
 import com.patrykandpatrick.vico.compose.chart.Chart
 import com.patrykandpatrick.vico.compose.chart.line.lineChart
+import com.patrykandpatrick.vico.core.entry.ChartEntryModelProducer
+import com.patrykandpatrick.vico.core.entry.entriesOf
 import com.patrykandpatrick.vico.core.entry.entryModelOf
+import com.patrykandpatrick.vico.core.entry.entryOf
+import kotlin.random.Random
 
 
 class MainActivity : ComponentActivity() {
@@ -38,12 +44,16 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun SimpleLineChart() {
     // Create data points for the chart
-    val model = entryModelOf(1f, 3f, 5f, 4f, 6f, 7f, 5f)
-
+    val model = entryModelOf(entriesOf(4f, 12f, 8f, 16f), entriesOf(12f, 16f, 4f, 12f))
+    fun getRandomEntries() = List(4) { entryOf(it, Random.nextFloat() * 16f) }
+    val chartEntryModelProducer = ChartEntryModelProducer(getRandomEntries())
+    
     // Display the line chart
     Chart(
         chart = lineChart(), // use barChart(), columnChart() for others
-        model = model
+        model = model,
+        startAxis = rememberStartAxis(),
+        bottomAxis = rememberBottomAxis(),
     )
 }
 
